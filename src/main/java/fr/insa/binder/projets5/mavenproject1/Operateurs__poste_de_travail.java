163911c931db1dffb30818e01a631f0e9fbd50d5
melan <melan@10.172.115.125>
2023-12-17 08:18:54 +0100

    gitignore

---
+++ src/main/java/fr/insa/binder/projets5/mavenproject1/Operateurs__poste_de_travail.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.binder.projets5.mavenproject1;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author melan
+ */
+public class Operateurs__poste_de_travail {
+    private int id_operateur;
+    private int id_poste_de_travail;
+
+    public Operateurs__poste_de_travail(int id_operateur, int id_poste_de_travail) {
+        this.id_operateur = id_operateur;
+        this.id_poste_de_travail = id_poste_de_travail;
+    }
+    
+    public void save_operations__poste_de_travail(Connection conn) throws SQLException {
+        try (PreparedStatement pst = conn.prepareStatement(
+                "insert operations_poste_de_travail_bof (id_operateur,id_poste_de_travail) values (?,?)")) {
+            pst.setInt(1, this.id_operateur);
+            pst.setInt(2, this.id_poste_de_travail);
+            pst.executeUpdate();
+        }
+    }
+    @Override
+    public String toString() {
+        return "Operations__poste_de_travail{" + "id_operateur=" + getId_operateur() + ", id_poste_de_travail=" + getId_poste_de_travail() + '}';
+    }
+
+    /**
+     * @return the id_operateur
+     */
+    public int getId_operateur() {
+        return id_operateur;
+    }
+
+    /**
+     * @return the id_poste_de_travail
+     */
+    public int getId_poste_de_travail() {
+        return id_poste_de_travail;
+    }
+    
+}
